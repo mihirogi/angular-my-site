6a0c1b9197c8fad1d8bb2622a33243c170e3c380
mihirogi <mihirogi0000@gmail.com>
2018-12-04 01:17:44 +0900

    とりあえずとれるようになった

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-art',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtComponent implements OnInit {
 
-  constructor() { }
+  private url = 'https://dozhzq3im0.execute-api.ap-northeast-1.amazonaws.com/dev/arts';
+  private response;
 
-  ngOnInit() {
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit() {
+    this.getArt();
+  }
+
+  getArt() {
+    this.http.get(this.url, this.httpOptions).subscribe(
+      res => {
+        console.log('response:');
+        console.log(res);
+      }
+    );
+  }
 }
